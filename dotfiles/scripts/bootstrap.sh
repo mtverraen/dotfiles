ac29981e5806aa6bc8b1f0cc65535ab9c9efe4d9
Martin Tverråen <martin.tveraen@evry.com>
2020-05-21 14:39:55 +0200

    add homebrew to bootstrapping script

--- dotfiles/scripts/bootstrap.sh
+++ dotfiles/scripts/bootstrap.sh
@@ -1,14 +1,17 @@
-#!/usd/bin/env zsh
+#!/usd/bin/env bash 
 
 #Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+#Install brew formulae and casks
+cat ./brew.sh ./.cask >> hb.sh
+chmod +x hb.sh
+./hb.sh
 
 #Install docker desktop for mac
 curl https://download.docker.com/mac/stable/Docker.dmg > Docker.dmg
 
 #curl dotfiles-repo and do everything
-
-#Install brew formulae and casks
 
 #Install omf
 #curl -L https://get.oh-my.fish | fish
